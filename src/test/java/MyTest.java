b0eba1744c77c4a1e103c445d30190b3e240e6a0
Turii <o.turij@gmail.com>
2024-02-03 10:03:23 -0800

    change pom and base_test created

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,10 +1,7 @@
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+
 
 public class MyTest {
-    @BeforeAll
+
     static void beforeAll() {
         System.out.println("beforeAll");
 
@@ -12,29 +9,29 @@ public class MyTest {
 
 
 
-    @AfterEach
+
     void tearDown() {
         System.out.println("tearDown");
         System.out.println("tearDfgjjgown");
 
     }
-    @AfterAll
+
     static void afterAll() {
         System.out.println("tafterAll");
 
     }
 
-    @Test
+
     public void test3(){
         System.out.println("test3");
     }
 
-    @Test
+
     public void test1(){
         System.out.println("test1");
     }
 
-    @Test
+
     public void test2(){
         System.out.println("test2");
     }
