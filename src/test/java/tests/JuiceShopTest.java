09e6eb66ff0130245eb93f2af4913779c2e6010e
Turii <44880961+Turii@users.noreply.github.com>
2024-02-03 20:26:01 -0800

    Test2 (#5)
    * Changed testLogin
    
    * Changed ymlfile
    
    * Changed test

--- src/test/java/tests/JuiceShopTest.java
+++ src/test/java/tests/JuiceShopTest.java
@@ -4,7 +4,6 @@ import model.JuiceShopLoginPage;
 import model.JuiceShopPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import utils.Utils;
 
 public class JuiceShopTest extends BaseTest{
     public static final String EMAIL = System.getenv("JUICE_SHOP_EMAIL");
@@ -13,7 +12,6 @@ public class JuiceShopTest extends BaseTest{
     @Test
     public void testLogin() {
         getDriver().get("https://www.saucedemo.com/");
-//        System.out.println("Email: ??????????????????????????????");
 //        String bb = new JuiceShopLoginPage(getDriver()).loginButtonNae();
 //        System.out.println("Email: ??????????????????????????????" + bb );
         String firstProduct = new JuiceShopLoginPage(getDriver())
