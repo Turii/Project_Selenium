2a4d74bce0a0f5b90274ef83f66844384086c0a7
Turii <o.turij@gmail.com>
2024-02-04 18:27:16 -0800

    Changed properties

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,15 +5,49 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import utils.Utils;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.time.Duration;
+import java.util.Enumeration;
+import java.util.Properties;
 
 public abstract class BaseTest {
     private WebDriver driver;
     private WebDriverWait wait5;
     private WebDriverWait wait10;
+    private static Properties properties = new Properties();
+
+
+    @BeforeSuite
+    protected void initProperties() {
+        boolean isServerRun = System.getenv("CI_RUN") != null;
+        if (isServerRun) {
+//            System.setProperty("JUICE_SHOP_EMAIL", System.getenv("JUICE_SHOP_EMAIL"));
+//            System.setProperty("JUICE_SHOP_PASSWORD", System.getenv("JUICE_SHOP_PASSWORD"));
+        } else {
+            try {
+                InputStream inputStream = BaseTest.class.getClassLoader().getResourceAsStream("local.properties");
+                if (inputStream == null) {
+                    System.out.println("ERROR: The \u001B[31mlocal.properties\u001B[0m file not found in src/test/resources/ directory.");
+                    System.out.println("You need to create it from local.properties.TEMPLATE file.");
+                    System.exit(1);
+                }
+                properties.load(inputStream);
+            } catch (IOException ignore) {
+            }
+
+            Enumeration<Object> keys = properties.keys();
+            while (keys.hasMoreElements()) {
+                String key = (String) keys.nextElement();
+                String value = properties.getProperty(key);
+                System.setProperty(key, value);
+            }
+        }
+    }
 
     @BeforeMethod
     protected void beforeMethod() {
--- src/test/java/tests/JuiceShopTest.java
+++ src/test/java/tests/JuiceShopTest.java
@@ -6,17 +6,16 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class JuiceShopTest extends BaseTest{
-    public static final String EMAIL = System.getenv("JUICE_SHOP_EMAIL");
-    public static final String PASSWORD = System.getenv("JUICE_SHOP_PASSWORD");
 
     @Test
     public void testLogin() {
+        final String EMAIL = System.getProperty("JUICE_SHOP_EMAIL");
+        final String PASSWORD = System.getProperty("JUICE_SHOP_PASSWORD");
+
         getDriver().get("https://www.saucedemo.com/");
         String firstProduct = new JuiceShopLoginPage(getDriver())
-                //.enterLogin(EMAIL)
-                .enterLogin("standard_user")
-                //.enterPassword(PASSWORD)
-                .enterPassword("secret_sauce")
+                .enterLogin(EMAIL)
+                .enterPassword(PASSWORD)
                 .loginUser()
                 .getFirstProductButton();
 
